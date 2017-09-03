9d09a5a501576a8496badba47e9e60849d467f41
Alarka Sanyal <alarkas@gmail.com>
2017-09-02 22:27:16 -0500

    Updated MinimalTree

--- src/com/examples/TreesAndGraphs/MinimalTree.java
+++ src/com/examples/TreesAndGraphs/MinimalTree.java
@@ -3,22 +3,22 @@ package com.examples.TreesAndGraphs;
 public class MinimalTree {
 
 	// Sorted array
-	private static int[] array = {0,1,2,3,4,5,6,7,8,9};
+	private int[] array = {0,1,2,3,4,5,6,7,8,9};
 	
 	public static void main(String[] args) {
 		MinimalTree mt = new MinimalTree();
-		Node head = mt.createMinimalTree(0, array.length - 1);
+		Node head = mt.createMinimalTree(mt.array, 0, mt.array.length - 1);
 		System.out.println("Done");
 	}
 	
-	private Node createMinimalTree(int start, int end) {
+	private Node createMinimalTree(int[] array, int start, int end) {
 		if (end < start) {
 			return null;
 		}
 		int mid = (start + end) / 2;
 		Node tempHead = new Node(array[mid]);
-		tempHead.left = createMinimalTree(start, mid - 1);
-		tempHead.right = createMinimalTree(mid + 1, end);
+		tempHead.left = createMinimalTree(array, start, mid - 1);
+		tempHead.right = createMinimalTree(array, mid + 1, end);
 		return tempHead;
 	}
 
